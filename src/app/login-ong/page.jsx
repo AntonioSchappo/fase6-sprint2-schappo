fa446f041433c00e5a75f26af73504aa2750a9dd
Guilherme <guilhermeavellar25@gmail.com>
2024-10-10 21:09:51 -0300

    conserto da imagem e fonte

--- src/app/login-ong/page.jsx
+++ src/app/login-ong/page.jsx
@@ -4,50 +4,49 @@ import Image from 'next/image';
 export default function loginOng () {
 
     return (
-        <div class="grid grid-cols-2 items-stretch">
-            <div class="">                
-                <Image 
+        <div className="grid grid-cols-2 items-stretch overflow-hidden">
+                           
+                <img 
                     src="/assets/images/login-ong.png"
                     alt="imagem da tela de login da ONG"
-                    width={500}
-                    height={500}              
+                    className="object-cover w-full h-screen"
                 />
                 {/* 
                     layout="fill"
                     objectFit="cover" 
                 */}
                 
-            </div>
+            
 
-            <div class="flex items-center justify-center text-black">
+            <div className="flex items-center justify-center text-black">
 
-                <div class="bg-white rounded-lg w-[488px] flex flex-col p-[36px]">
-                    <h2 class="text-[40px] leading-[120%] mb-8">Área da ONG</h2>
-                    <form action="" class="w-full flex flex-col">
-                        <label htmlFor="email" class="mb-4">E-mail</label>
-                        <input type="text" class="border border-solid rounded-sm p-[16px] mb-[24px]" placeholder="Insira seu email" id="email"/>
+                <div className="bg-white rounded-lg w-[488px] flex flex-col p-[36px]">
+                    <h2 className="text-[40px] leading-[120%] mb-8 font-geistsans">Área da ONG</h2>
+                    <form action="" className="w-full flex flex-col">
+                        <label htmlFor="email" className="mb-4">E-mail</label>
+                        <input type="text" className="border border-solid rounded-sm p-[16px] mb-[24px]" placeholder="Insira seu email" id="email"/>
 
-                        <label htmlFor="password" class="mb-4">Senha</label>
-                        <input type="password" class="border border-solid rounded-sm p-[16px] mb-[24px]" placeholder="Insira sua senha" id="password"/>
+                        <label htmlFor="password" className="mb-4">Senha</label>
+                        <input type="password" className="border border-solid rounded-sm p-[16px] mb-[24px]" placeholder="Insira sua senha" id="password"/>
 
-                        <a href="" class="flex justify-end">
+                        <a href="" className="flex justify-end">
                             <Image 
                                 src="/assets/icons/cadeado.svg"
                                 alt="icone de cadeado"
                                 width={20}
                                 height={20}
                             />
-                            <p class="ml-4 font-bold">Esqueci minha senha</p>
+                            <p className="ml-4 font-bold">Esqueci minha senha</p>
                         </a>
 
                         {/* 
                             Button
                             componente a ser criado? 
                         */}
-                        <button class="bg-[#FF9800] hover:bg-[#C26100] px-[21px] py-[14px] my-4 rounded-sm text-white">Entrar</button>
+                        <button className="bg-[#FF9800] hover:bg-[#C26100] px-[21px] py-[14px] my-4 rounded-sm text-white">Entrar</button>
 
-                        <div class="self-center text-[18px]">
-                            <p class="font-light">Não tem uma conta? <a href="" class="font-bold">Crie uma conta de graça</a></p>
+                        <div className="self-center text-[18px]">
+                            <p className="font-light">Não tem uma conta? <a href="" className="font-bold">Crie uma conta de graça</a></p>
                         </div>
 
                     </form>

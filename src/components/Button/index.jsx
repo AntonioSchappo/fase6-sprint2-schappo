26cff56dcc47339b0a48768a20eada14183fc081
@pedrodecf <contato.pedrodecf@gmail.com>
2024-11-10 19:37:22 -0300

    fix styles for button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -12,9 +12,9 @@ export function Button({
   ...props
 }) {
   const primaryStyle =
-    "bg-[#FF9800] hover:bg-[#C26100] text-white font-bold py-2 px-6 rounded duration-200 ease-in-out hover:shadow-lg font-nunito-sans";
+    "bg-[#FF9800] hover:bg-[#C26100] text-white font-bold py-2 px-6 rounded duration-200 ease-in-out font-nunito-sans";
   const outlineStyle =
-    "border font-bold border-[#FF9800] text-[#FF9800] py-2 px-6 rounded hover:border-[#C26100] hover:text-[#C26100] duration-200 ease-in-out hover:shadow-lg font-nunito-sans";
+    "border font-bold border-[#FF9800] text-[#FF9800] py-2 px-6 rounded hover:border-[#C26100] hover:text-[#C26100] duration-200 ease-in-out font-nunito-sans";
   const buttonStyle = variant === "outline" ? outlineStyle : primaryStyle;
 
   return (
